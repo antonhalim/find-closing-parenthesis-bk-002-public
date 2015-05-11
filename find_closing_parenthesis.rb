02059d81d25e98d74bc98a9df198027326a99923
axton21 <axton_n@yahoo.com>
2015-05-11 16:53:59 -0400

    done

--- find_closing_parenthesis.rb
+++ find_closing_parenthesis.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def find_closing_parenthesis(sentence, open_position)
-
+	opening_count = 0
+	curl_index = 0
+	sentence.each_char do |curl|
+		if curl == "("
+			opening_count += 1
+			curl_index = sentence.index(/[\)]/,curl_index+1)
+		end
+		if opening_count == open_position
+			return sentence.index(/[\)]/,curl_index)
+		end
+	end
 end
